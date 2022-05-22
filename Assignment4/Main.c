934dbfba1a3fa769fb4ed19f67d89cface257c1d
Dima-Sandler <dima1.san@gmail.com>
2022-05-22 23:31:41 +0300

    Update

--- Assignment4/Main.c
+++ Assignment4/Main.c
@@ -27,7 +27,6 @@ typedef struct Student
 void fcheck(FILE*, _Bool), rtoa(char*, char**), freeStrings(char** arr, int size), freeStudents(char***, int), * xmalloc(unsigned int);
 int constrainGrade(int);
 
-// Part A
 void printStudentArray(const char* const* const* students, const int* coursesPerStudent, int numberOfStudents);
 void countStudentsAndCourses(const char* fileName, int** coursesPerStudent, int* numberOfStudents);
 void factorGivenCourse(char** const* students, const int* coursesPerStudent, int numberOfStudents, const char* courseName, int factor);
@@ -36,14 +35,8 @@ void studentsToFile_s(char*** students, int* coursesPerStudent, int numberOfStud
 char*** makeStudentArrayFromFile(const char* fileName, int** coursesPerStudent, int* numberOfStudents);
 int countPipes(const char* lineBuffer, int maxCount);
 
-// Part B
-void writeToBinFile(const char* fileName, Student* students, int numberOfStudents);
-Student* readFromBinFile(const char* fileName);
-Student* transformStudentArray(char*** students, const int* coursesPerStudent, int numberOfStudents);
-
 int main()
 {
-	// Part A
 	int numberOfStudents = 0;
 	int* coursesPerStudent = NULL;		
 	char*** students = makeStudentArrayFromFile("studentList.txt", &coursesPerStudent, &numberOfStudents);
@@ -52,20 +45,9 @@ int main()
 	printStudentArray(students, coursesPerStudent, numberOfStudents);	
 	studentsToFile(students, coursesPerStudent, numberOfStudents); // this frees all memory. Part B fails if this line runs. uncomment for testing (and comment out Part B)
 
-	// Part B
-	/*Student* transformedStudents = transformStudentArray(students, coursesPerStudent, numberOfStudents);
-	writeToBinFile("students.bin", transformedStudents, numberOfStudents);
-	Student* testReadStudents = readFromBinFile("students.bin");*/
-
-	// add code to free all arrays of struct Student
-
-	/*_CrtDumpMemoryLeaks();*/ // uncomment this line to check for heap memory allocation leaks.
-	// Read https://docs.microsoft.com/en-us/visualstudio/debugger/finding-memory-leaks-using-the-crt-library?view=vs-2019
-
 	return 0;
 }
 
-// Part A
 void printStudentArray(const char* const* const* students, const int* coursesPerStudent, int numberOfStudents)
 {
 	for (int i = 0; i < numberOfStudents; i++)
@@ -397,20 +379,6 @@ int countPipes(const char* lineBuffer, int maxCount)
 			
 	return count;
 }	
-
-// Part B
-void writeToBinFile(const char* fileName, Student* students, int numberOfStudents)
-{
-	//add code here
-}
-Student* readFromBinFile(const char* fileName)
-{
-	//add code here
-}
-Student* transformStudentArray(char*** students, const int* coursesPerStudent, int numberOfStudents)
-{
-	//add code here
-}
 
 // auxiliary functions
 void fcheck(FILE* pFile, _Bool op)
